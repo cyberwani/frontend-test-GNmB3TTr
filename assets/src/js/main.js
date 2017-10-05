d652f7a1395d29b9f7273b927e72ae8533f97557
Wes Cole <wesley.cole87@gmail.com>
2017-10-04 22:34:23 -0400

    challenge submission

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,3 +1,9 @@
 ( function() {
-	console.log( 'A foo walks into a bar, takes a look around and says "Hello World!"' );
-})() ;
+	var authorBio = document.getElementById( 'author-card__bio' );
+	var authorBioTrigger = document.getElementById( 'author-card__trigger' );
+
+	authorBioTrigger.addEventListener( 'click', e => {
+		e.preventDefault();
+		authorBio.classList.toggle( 'author-card__bio--is-open' );
+	});
+})();
